b561019089aef03f681f2c8c8960a07915bbe136
AngelicaCorrales <angelicacorrales2002@gmail.com>
2021-06-10 10:21:52 -0500

    Javadoc modified

--- src/model/RectangleFigure.java
+++ src/model/RectangleFigure.java
@@ -4,6 +4,14 @@ public class RectangleFigure {
 	private double xCoordinate;
 	private double yCoordinate;
 	
+	/**
+	*This is the constructor of the class. <br>
+	*<b>name:</b> RectangleFigure. <br>
+	*<b>pre</b>: the variables x, y, are already initialized. <br>
+	*<b>post:</b> the attributes and relationships of the class have been initialized.<br>
+	*@param x Is an integer variable that indicates the x coordinate<br>
+	*@param y Is an integer variable that indicates the y coordinate<br>
+	*/
 	public RectangleFigure(double x, double y) {
 		setxCoordinate(x);
 		setyCoordinate(y);
